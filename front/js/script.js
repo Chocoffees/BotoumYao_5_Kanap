b57452ef89fb2c3e8e98a2a1fcdf3a3d42d855b9
Chocoffees <sirogrenadine@gmail.com>
2021-12-02 11:14:50 +0100

    Homepage updated: function added

--- front/js/script.js
+++ front/js/script.js
@@ -1,26 +1,25 @@
 // --- Get products from API ---
+getAllProducts();
 
-//API Request
-fetch('http://localhost:3000/api/products')
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        }
-    })
+function getAllProducts() {
+    // API Request
+    fetch('http://localhost:3000/api/products')
+        .then(response => response.json()
 
-//Insert each product details on homepage
-    .then(productList => {
-        console.log(productList);
-        for (let product of productList) {
-            document.querySelector(".items").innerHTML += `<a href="./product.html?id=${product._id}">
+            // Insert each product details on homepage
+            .then(productList => {
+                console.log(productList);
+                for (let product of productList) {
+                    document.querySelector(".items").innerHTML += `<a href="./product.html?id=${product._id}">
             <article>
               <img src="${product.imageUrl}" alt="${product.altTxt}, ${product.name}">
               <h3 class="productName">${product.name}</h3>
               <p class="productDescription">${product.description}</p>
             </article>
           </a>`;
-        }
-    })
-    .catch(function(error) {
-        console.log('error');
-    })
+                }
+            }))
+        .catch(error => {
+            console.log('error');
+        })
+}
